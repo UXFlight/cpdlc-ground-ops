744d54e0e054bdeacd4203334d9813ef25ae3d7b
simy46 <missipsa.annane@polymtl.ca>
2026-01-26 19:44:26 -0500

    Add metrics support to SocketService for event emission tracking

--- app/classes/socket.py
+++ app/classes/socket.py
@@ -1,14 +1,18 @@
 from flask_socketio import SocketIO, join_room, leave_room
+from typing import Optional, Any
 
 class SocketService:
-    def __init__(self, socketio): 
+    def __init__(self, socketio, metrics: Optional[Any] = None): 
         self.socketio : SocketIO = socketio
+        self.metrics = metrics
 
     def listen(self, event_name, callback):
         self.socketio.on(event_name)(callback)
 
     def send(self, event, data, room=None): 
         self.socketio.emit(event, data, to=room)
+        if self.metrics:
+            self.metrics.record_emit(event, room)
 
     def enter_room(self, sid, room):
         join_room(room, sid=sid)
