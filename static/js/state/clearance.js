4fb9395686f0cbfdc2be3aa398c6bddaf4c29426
simy46 <missipsa.annane@polymtl.ca>
2026-04-12 15:01:11 +0200

    koding like krazy

--- static/js/state/clearance.js
+++ static/js/state/clearance.js
@@ -9,14 +9,8 @@ export const setClearance = (kind, instruction) => {
 }
 
 export const getRecentClearance = () => {
-  if (clearance.route_change) {
-    return { kind: "route_change", instruction: clearance.route_change };
-  }
-  if (clearance.taxi) {
-    return { kind: "taxi", instruction: clearance.taxi };
-  }
-  if (clearance.expected) {
-    return { kind: "expected", instruction: clearance.expected };
-  }
+  if (clearance.route_change) return { kind: "route_change", instruction: clearance.route_change };
+  if (clearance.taxi) return { kind: "taxi", instruction: clearance.taxi };
+  if (clearance.expected)  return { kind: "expected", instruction: clearance.expected };
   return null;
 };
